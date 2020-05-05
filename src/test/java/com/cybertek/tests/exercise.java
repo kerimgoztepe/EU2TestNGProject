2482b7a515f45b6880e8632ddc25e216307b93e7
LENOVO <h_iltas@yahoo.com>
2020-05-05 13:49:45 +0300

    OMG

--- src/test/java/com/cybertek/tests/exercise.java
+++ src/test/java/com/cybertek/tests/exercise.java
@@ -5,39 +5,49 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
 public class exercise {
 
     WebDriver driver;
-
-    @BeforeTest
-    public void setUp() {
+    @BeforeMethod
+    public void setUpMethod() {
         driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/tables");
     }
-
-    @AfterTest
-    public void closeUp() throws InterruptedException {
+    @AfterMethod
+    public void afterMethod() throws InterruptedException {
         Thread.sleep(3000);
         driver.quit();
     }
-//Loop içinde geçişler yok sadece ilk resim için işlem yapıyor???
     @Test
-    public void test() throws InterruptedException {
-
-        driver.get("http://practice.cybertekschool.com/hovers");
-        for (int i = 0; i < 4; i++) {
-            WebElement img1 = driver.findElement(By.tagName("img"));
-            Actions actions = new Actions(driver);
-            Thread.sleep(2000);
-            actions.moveToElement(img1).perform();
-
-        }
+    public void PrintAsingleCellByMethod() throws InterruptedException {
+/*
+        Scanner input = new Scanner(System.in);
+        System.out.print( "Please enter a Column number : ");
+        int columnNum = input.nextInt();
+        System.out.print( "Please enter a Row number : ");
+        int rowNum = input.nextInt();
+*/
+        int columnNum = 4;
+        int rowNum = 4;
+        System.out.println("Automation will display the cell in COLUMN : " + columnNum+ " ROW : " + rowNum);
+        String CellData = callAcell(columnNum,rowNum).getText();
+        //this part could be directly in soutv without declaring CellData variable : Lazy way
+        System.out.println("The  cell  contains data  : "  + CellData);
+    }
+    //create a method  having two parameters representing the column and row numbers to call a single cell
+    private WebElement callAcell(int i, int j){
+        //define the xPath of a cell in a dynamic way
+        String cellXpath = "//table[@id='table1']/tbody/tr["+i+"]/td["+j+"]";
+        //define a WebElement representing the cell in given column and row
+        WebElement singleCell = driver.findElement(By.xpath(cellXpath));
+        //return the WebElement representing the cell in given column and row
+        return singleCell;
     }
 }
 
