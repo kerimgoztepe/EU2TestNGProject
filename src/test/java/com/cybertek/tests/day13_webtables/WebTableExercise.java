ab8b37809f31d3796d8a6da8552b3f8317f0cc43
hiltas73 <h_iltas@yahoo.com>
2020-05-06 21:21:15 +0300

    OMG

---
+++ src/test/java/com/cybertek/tests/day13_webtables/WebTableExercise.java
@@ -0,0 +1,63 @@
+package com.cybertek.tests.day13_webtables;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class WebTableExercise {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/tables");
+
+    }
+
+    @AfterMethod
+    public void afterMethod() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+    @Test
+    public void test1(){
+        driver.get("http://practice.cybertekschool.com/tables");
+
+        //table 2 index numbers for data part excluding header
+        int rowNo = getNoOfRows();
+        int colNo = getNoOfCols();
+
+        for (int i = 1; i < 5; i++) {
+
+        }
+        List<WebElement> elements = driver.findElements(By.xpath("//table[@id='table2']//th"));
+
+        for (WebElement element : elements) {
+           System.out.println("element = " + element.getText());
+        }
+        
+    }
+
+    private int getNoOfCols() {
+        //locate all columns
+        return 0;
+    }
+
+    private int getNoOfRows() {
+        //locate all rows as webelement assign to List elements
+        List<WebElement> noOfRows = driver.findElements(By.xpath("//table[@id='table2']//th"));
+        //return no of rows as int number
+        return noOfRows.size();
+    }
+}
