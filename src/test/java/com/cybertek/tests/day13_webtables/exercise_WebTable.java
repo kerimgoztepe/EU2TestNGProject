0cb43ec5b4c04884676e2a05b27c9c05621fdf35
hiltas73 <h_iltas@yahoo.com>
2020-05-06 23:05:50 +0300

    OMG

--- src/test/java/com/cybertek/tests/day13_webtables/exercise_WebTable.java
+++ src/test/java/com/cybertek/tests/day13_webtables/exercise_WebTable.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -36,7 +37,37 @@ public class exercise_WebTable {
         for (WebElement element : elements) {
             System.out.println(element.getText());
         }
+    }
+    @Test
+    public void test2(){
+        int rows = getRows();
+        int columns = getColumns();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                String tableItem = "//table[@id='table1']//tbody/tr[" + i + "]/td[" + j + "]";
+                String wantedItem = "//table[@id='table1']//tbody/tr[" + i + "]";
+
+                WebElement element = driver.findElement(By.xpath(tableItem));
+                if (element.getText().equals("Jason")){
+                    WebElement target = driver.findElement(By.xpath(wantedItem));
+                    System.out.println("Jason info = " + target.getText());
+                }
 
 
+
+            }
+
+        }
+    }
+
+    private int getRows() {
+        List<WebElement> rowsNo = driver.findElements(By.xpath("//table[@id='table1']//tbody/tr"));
+        return rowsNo.size();
+    }
+
+    private int getColumns() {
+        List<WebElement> columnNo = driver.findElements(By.xpath("//table[@id='table1']//thead/tr/th"));
+        return columnNo.size();
     }
 }
