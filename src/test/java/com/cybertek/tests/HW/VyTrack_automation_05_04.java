1eddabb246dc3ad9c20eb9d63d6f18edc0f729eb
hiltas73 <h_iltas@yahoo.com>
2020-05-10 02:10:07 +0300

    OMG

--- src/test/java/com/cybertek/tests/HW/VyTrack_automation_05_04.java
+++ src/test/java/com/cybertek/tests/HW/VyTrack_automation_05_04.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -27,7 +29,7 @@ public class VyTrack_automation_05_04 {
     @AfterMethod
     public void afterMethod() throws InterruptedException {
         Thread.sleep(3000);
-        //driver.quit();
+        driver.quit();
     }
 
    @Test
@@ -62,16 +64,20 @@ public class VyTrack_automation_05_04 {
        Assert.assertTrue(actualVehicleInfo.contentEquals(expectedVehicleInfo),"verify vehicle info");
        System.out.println("expectedVehicleInfo = " + expectedVehicleInfo);
        System.out.println("actualVehicleInfo = " + actualVehicleInfo);
+       Thread.sleep(3000);
 
 //       verify that you edit one vehicle contract info add new info and verify you made changes
 
        //locate edit button and click
        WebElement editButton = driver.findElement(By.xpath("//a[@title='Edit Vehicle Contract']"));
-       editButton.click(); //we can also do this action in previous line
+       Actions actions = new Actions(driver);
+       actions.moveToElement(editButton).click().perform();
+       Thread.sleep(2000);
+
        //locate VehiclesModel button and click
        WebElement VehiclesModelButton = driver.findElement(By.xpath("//a[.='VehiclesModel']"));
+
        //use actions to click on VehiclesModel button
-       Actions actions = new Actions(driver);
        actions.moveToElement(VehiclesModelButton).click().perform();
        Thread.sleep(2000);
 
@@ -79,28 +85,36 @@ public class VyTrack_automation_05_04 {
        WebElement AddButton1 = driver.findElement(By.xpath("(//button[@type='button'])[1]"));
        AddButton1.click();
        Thread.sleep(2000);
-       //handle the new window
-       /*String currentWindow = driver.getWindowHandle();
-       Set<String> windowHandles = driver.getWindowHandles();
-       System.out.println("windowHandles = " + windowHandles.size());
-       for (String handle : windowHandles) {
-           if (!handle.equals(windowHandles)){
-               driver.switchTo().window(handle);
-           }
-       }*/
+
        //locate any element (Accord) and click on it then click on select button
        WebElement checkBoxItem = driver.findElement(By.xpath("//tbody/tr[12]/td[1]"));
        checkBoxItem.click();
        Thread.sleep(2000);
        driver.findElement(By.xpath("(//*[.='Select'])[2]")).click();
        Thread.sleep(2000);
-       //switch first window
-       //driver.switchTo().window(currentWindow);
+
        //locate and verify model name is displayed
        String expectedName = "Model Name: Accord";
        WebElement modelName = driver.findElement(By.xpath("//*[.='Model Name: Accord']"));
        Assert.assertTrue(modelName.isDisplayed(),"verify that model name is displayed");
        Assert.assertTrue(expectedName.contentEquals(modelName.getText()),"verify that modelName is equal to our expected name");
+       System.out.println("expectedName = " + expectedName);
+       System.out.println("modelName = " + modelName.getText());
+
+       //locate save and close button and click
+       driver.findElement(By.xpath("(//button[@type='submit'])[1]")).click();
+
+       //waiting for the loader is gone
+       WebDriverWait wait = new WebDriverWait(driver, 10);
+       WebElement loader= driver.findElement(By.cssSelector(".loader-mask>.loader-frame"));
+       wait.until(ExpectedConditions.invisibilityOfAllElements(loader));
+
+       //locate and verify change message is displayed (be careful just a few seconds then message is gone
+       WebElement flashMessage= driver.findElement(By.xpath("//div[@class='flash-messages-holder']"));
+       wait.until(ExpectedConditions.visibilityOf(flashMessage));
+       System.out.println("flashMessage = " + flashMessage.getText());
+
+       Assert.assertTrue(flashMessage.isDisplayed(), "add vehicle model message is displayed");
 
 
    }
