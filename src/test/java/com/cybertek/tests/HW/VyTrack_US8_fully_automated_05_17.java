0bd0f09e75af39b1b026a34bfe3e8e2bc2925d10
hiltas73 <h_iltas@yahoo.com>
2020-05-20 00:11:54 +0300

    update tuesday night

--- src/test/java/com/cybertek/tests/HW/VyTrack_US8_fully_automated_05_17.java
+++ src/test/java/com/cybertek/tests/HW/VyTrack_US8_fully_automated_05_17.java
@@ -1,10 +1,12 @@
 package com.cybertek.tests.HW;
 
 import com.cybertek.pages.BasePage;
+import com.cybertek.pages.DashboardPage;
 import com.cybertek.pages.LoginPage;
 import com.cybertek.tests.TestBase;
 import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.ConfigurationReader;
+import com.google.common.base.Verify;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -14,38 +16,38 @@ import org.testng.annotations.Test;
 public class VyTrack_US8_fully_automated_05_17 extends TestBase {
 
 
-
     /***
      * AC1: Verify that authorized user should be able to access Vehicle Contract
      * and able to see all vehicle contracts on the grid
      */
 
-    /*@Test (description = "AC1")
+    @Test(description = "AC1")
     public void AC1() {
         //navigate to "https://qa1.vytrack.com/entity/Extend_Entity_VehicleContract"
         //Use credentials to login (Before method in TestBase)
 
         //hover over to Fleet module
         //Hover over to Vehicle Contracts module and click
-
-        //BasePage basePage = new BasePage();
-        navigateToModule("Fleet", "Vehicle Contracts");
+        DashboardPage dashboardPage = new DashboardPage();
+        dashboardPage.navigateToModule("Fleet", "Vehicle Contracts");
 
         //wait until loading completes
         //wait.until(ExpectedConditions.invisibilityOfAllElements(loader));
         BrowserUtils.waitForPageToLoad(10);
 
         //get title and print
-        String pageSubTitle = getPageSubTitle();
+        String pageSubTitle = dashboardPage.getPageSubTitle();
 
         //verify to see all vehicle contracts as a list
         Assert.assertEquals(pageSubTitle, "All Vehicle Contract", "verify to access Vehicle Contract and see list");
 
 
     }
-    *//***
+
+    /***
      * AC2: Verify that authorized user should be able to create Vehicle contract
-     *//*
+     */
+
     @Test (description = "AC2")
     public void AC2(){
 
@@ -55,7 +57,8 @@ public class VyTrack_US8_fully_automated_05_17 extends TestBase {
 
         //hover over to Fleet module
         //Hover over to Vehicle Contracts module and click
-        navigateToModule("Fleet", "Vehicle Contracts");
+        DashboardPage dashboardPage = new DashboardPage();
+        dashboardPage.navigateToModule("Fleet", "Vehicle Contracts");
 
         //wait until loading completes
         BrowserUtils.waitForPageToLoad(10);
@@ -63,5 +66,5 @@ public class VyTrack_US8_fully_automated_05_17 extends TestBase {
         //Click on Create Vehicle Contract button
 
 
-    }*/
+    }
 }
