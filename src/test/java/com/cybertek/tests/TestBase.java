4985cbd2f679372d363b3ca4d2ed51aba951f872
hiltas73 <h_iltas@yahoo.com>
2020-05-26 23:11:29 +0300

    Tuesday night after class

--- src/test/java/com/cybertek/tests/TestBase.java
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -4,7 +4,6 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
-import com.cybertek.pages.LoginPage;
 import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
@@ -12,10 +11,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -27,11 +23,12 @@ public class TestBase {
     protected WebDriverWait wait;
 
     //this class is used for starting and building reports
-    protected ExtentReports report;
+    protected static ExtentReports report;
     //this class is used to create HTML report file
-    protected ExtentHtmlReporter htmlReporter;
+    protected static ExtentHtmlReporter htmlReporter;
     //this will define a test, enables adding logs, authors, test steps
-    protected ExtentTest extentLogger;
+    protected static ExtentTest extentLogger;
+    protected String url;
 
     @BeforeTest
     public void setUpTest() {
@@ -49,7 +46,7 @@ public class TestBase {
         report.attachReporter(htmlReporter);
 
         //title in report
-        htmlReporter.config().setReportName("App Smoke Test");
+        htmlReporter.config().setReportName("Vytrack Smoke Test");
 
         //set environment info
         report.setSystemInfo("Environment", "QA");
@@ -58,13 +55,23 @@ public class TestBase {
     }
 
     @BeforeMethod
-    public void setUpMethod() {
+    @Parameters("env")
+    public void setUpMethod(@Optional String env) {
+        System.out.println("env = " + env);
+
+        //if env is null use default url
+        if (env == null){
+            url = ConfigurationReader.get("url");
+        }else{
+            url = ConfigurationReader.get(env+"_url");
+        }
+        //if it is not null, choose env based on value
         driver = Driver.get();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         actions = new Actions(driver);
         wait = new WebDriverWait(driver, 10);
-        driver.get(ConfigurationReader.get("url"));
+        driver.get(url);
 
 
     }
