95d60582244473a3174cdff3e1e70289dd10c083
hiltas73 <h_iltas@yahoo.com>
2020-05-28 14:13:39 +0300

    Thursday afternoon

--- src/test/java/com/cybertek/tests/HW/HW_05_21.java
+++ src/test/java/com/cybertek/tests/HW/HW_05_21.java
@@ -227,7 +227,7 @@ public class HW_05_21 extends TestBase {
             for (int k = 0; k < calendarEventsPage.tableRows.size(); k++) {
                 //System.out.println("row is selected = " + calendarEventsPage.tableRows.get(k).getAttribute("class").contains("row-selected"));
 
-                Assert.assertTrue(calendarEventsPage.tableRows.get(k).getAttribute("class").contains("row-selected"), "verify page no:"+pageNoAsInt+" and "+calendarEventsPage.tableRows.get(k).getText()+ " is selected");
+                Assert.assertTrue(calendarEventsPage.tableRows.get(k).getAttribute("class").contains("row-selected"), "verify page no:"+i+" and "+calendarEventsPage.tableRows.get(k).getText()+ " is NOT selected");
 
                 /*if (!calendarEventsPage.tableRows.get(k).getAttribute("class").contains("row-selected")) {
                     System.out.println("page no:" + pageNoAsInt + " and row no:" + calendarEventsPage.tableRows.get(calendarEventsPage.tableRows.size()) + " is NOT selected");                    //calendarEventsPage.rightArrow.click();
