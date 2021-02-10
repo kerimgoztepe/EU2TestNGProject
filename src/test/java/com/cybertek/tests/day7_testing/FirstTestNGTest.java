6fba0a4013c6bdf0caca06539834952a6ba0b5e0
hiltas73 <h_iltas@yahoo.com>
2021-02-10 11:55:10 +0300

    Feb 10

--- src/test/java/com/cybertek/tests/day7_testing/FirstTestNGTest.java
+++ src/test/java/com/cybertek/tests/day7_testing/FirstTestNGTest.java
@@ -5,8 +5,10 @@ import org.testng.annotations.Test;
 
 public class FirstTestNGTest {
 
+
     @Test
     public void test1(){
+
         System.out.println("First Test");   // test code here...
     }
 
